14583701e6ec3465a3f61b970b2987971381bd5c
Harshit N Sharma <harshitpro@icloud.com>
2021-08-28 18:57:00 +0530

    Working sidebar ADDED

--- contactmanager/src/main/java/com/manager/config/MyConfig.java
+++ contactmanager/src/main/java/com/manager/config/MyConfig.java
@@ -46,7 +46,9 @@ public class MyConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN")
 		.antMatchers("/user/**").hasRole("USER")
-		.antMatchers("/**").permitAll().and().formLogin().loginPage("/signin").and().csrf().disable();
+		.antMatchers("/**").permitAll().and().formLogin().loginPage("/signin")
+		.defaultSuccessUrl("/user/index").failureUrl("/failed")
+		.and().csrf().disable();
 	}
 	
 	
--- contactmanager/src/main/java/com/manager/controller/UserController.java
+++ contactmanager/src/main/java/com/manager/controller/UserController.java
@@ -1,15 +1,30 @@
 package com.manager.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.manager.dao.UserRepository;
+import com.manager.entities.User;
 
 @Controller
 @RequestMapping("/user")
 public class UserController 
 {
-     @RequestMapping("/index")
-     public String dashboard()
+	@Autowired
+	 private UserRepository userRepository;
+     @RequestMapping(value="/index", method = RequestMethod.GET)
+     public String dashboard(Model model,Principal principal)
      {
+    	 String username=principal.getName();
+    	 System.out.println(username);
+    	 User user=userRepository.getUserByUserName(username);
+    	 model.addAttribute("user", user);
     	 return "user/user_dashboard";
      }
 }
+ 
